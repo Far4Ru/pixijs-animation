def49036485c99aecdc649343b27c6be0a2da78a
Far4Ru <faty422@mail.ru>
2023-03-05 23:04:05 +0300

    Добавлен полноэкранный режим

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,10 @@ import { BouncingName } from './bouncingname'
 
 export class Game {
 
+    private width = 1000;
+    private height = 600;
+    private isFullscreen = false;
+
     // Fields
     private bouncingName: BouncingName
     private _pixi: PIXI.Application
@@ -13,8 +17,49 @@ export class Game {
 
     constructor() {
 
-        this._pixi = new PIXI.Application({ width: 1000, height: 600, backgroundColor: 0xbe4d25 })
+        this._pixi = new PIXI.Application({ width: this.width, height: this.height, backgroundColor: 0xbe4d25 })
         document.getElementById("app").appendChild(this._pixi.view)
+        // Create the background sprite with a basic white texture
+        let bg = new PIXI.Sprite(PIXI.Texture.WHITE);
+        // Set it to fill the screen
+        bg.width = this._pixi.screen.width;
+        bg.height = this._pixi.screen.height;
+        bg.tint = 0xbe4d25;
+        // Add a click handler
+        bg.interactive = true;
+        bg.on('click', function(){
+            if (!this.isFullscreen) {
+                if(document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if(document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if(document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen();
+                } else if(document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                }
+                document.body.style.alignItems = "unset";
+                document.getElementById("app").style.width = "unset";
+                document.getElementById("app").style.maxWidth = "unset";
+                this.isFullscreen = true;
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+                document.body.style.alignItems = "center";
+                document.getElementById("app").style.width = "1280px";
+                document.getElementById("app").style.maxWidth = "1280px";
+                this.isFullscreen = false;
+            }
+        });
+        // Add it to the stage as the first object
+        this._pixi.stage.addChild(bg);
 
         new Assets(this)
 
